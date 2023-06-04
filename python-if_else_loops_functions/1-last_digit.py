d19c1d8ad5c2951a857aaa40c082652b740bc317
Johana-RHP <johana.rosicel@gmail.com>
2023-06-03 21:53:50 -0700

    fix: correction when executing the negative number module

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,12 +3,13 @@ import random
 number = random.randint(-10000, 10000)
 last_digit = number % 10
 string = f"Last digit of {number} is "
-if number >= 0:
-    if last_digit > 5:
-        print(f"{string}{last_digit} and is greater than 5")
-    elif last_digit == 0:
-        print(f"{string}{last_digit} and is 0")
-    else:
-        print(f"{string}{last_digit} and is less than 6 and not 0")
-elif number < 0:
-    print(f"{string}{last_digit * -1} and is less than 6 and not 0")
+if number < 0:
+    number = abs(number)
+    last_digit = (number % 10) * -1
+
+if last_digit > 5:
+    print(f"{string}{last_digit} and is greater than 5")
+elif last_digit == 0:
+    print(f"{string}{last_digit} and is 0")
+else:
+    print(f"{string}{last_digit} and is less than 6 and not 0")
