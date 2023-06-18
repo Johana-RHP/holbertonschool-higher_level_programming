8f60573d577902dda874e5c8900c82a18b429119
Johana-RHP <johana.rosicel@gmail.com>
2023-06-18 15:11:08 -0700

    feat: add the private instance Attribute size to the class square

---
+++ python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Square"""
+
+
+class Square:
+    """
+    Define a square
+    Args:
+        size : size of a side in square
+    """
+    def __init__(self, size):
+        """Initializes square"""
+        self.__size = size
