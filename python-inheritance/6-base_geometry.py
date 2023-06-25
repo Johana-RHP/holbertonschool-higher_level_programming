6563becd8073519f05ac85a4ca935eba59891eff
Johana-RHP <johana.rosicel@gmail.com>
2023-06-25 15:44:43 -0700

    feat: empty class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Module that create a class
+"""
+
+
+class BaseGeometry:
+    """ Empty class """
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
