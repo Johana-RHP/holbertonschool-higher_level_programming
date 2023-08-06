f27d9150a985fa68d52d7c46a9e8fdeb909270c2
Johana-RHP <johana.rosicel@gmail.com>
2023-08-06 15:24:22 -0700

    variable name correction

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 '''script that lists all the data in a table'''
-import MySQLdb as mysql
-from sys import argv
 
+from sys
+import MySQLdb
 
 if __name__=="__main__":
-    db = mysql.connect(user=argv[1], passwd=argv[2], db=argv[3], host="localhost", port=3306)
-    cur = conn.cursor()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306)
+    cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     query_rows = cur.fetchall()
     for state in query_rows:
         print(state)
     cur.close()
-    conn.close()
+    db.close()
