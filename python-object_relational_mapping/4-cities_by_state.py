f2f6aa087b8fb6af2e29a10f4ecc97d2a2568e21
Johana-RHP <johana.rosicel@gmail.com>
2023-08-10 13:08:55 -0700

    fix: pycodestyle

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
                 JOIN states ON cities.state_id = states.id \
                 ORDER BY cities.id")
-    
+
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
