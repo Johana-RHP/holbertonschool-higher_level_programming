b983e14c9ec01f73e0cc9a9f16312c37ad317629
Johana-RHP <johana.rosicel@gmail.com>
2023-08-10 08:52:45 -0700

    add task 4

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Defines a Rectangle class"""
+
+
+class Rectangle:
+    """ Initialize rectangles """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Getter returns width """
+        return self.__width
+
+    @property
+    def height(self):
+        """ Getter returns height """
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        res = []
+        for M in range(self.__height):
+            for S in range(self.__width):
+                res.append("#")
+            if M != self.__height - 1:
+                res.append("\n")
+        return ("".join(res))
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
