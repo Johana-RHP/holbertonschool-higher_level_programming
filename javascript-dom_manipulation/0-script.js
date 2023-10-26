b53ce4a9ad5cbb44166afc90e9863587acc79953
Johana-RHP <johana.rosicel@gmail.com>
2023-10-25 23:16:30 -0700

    task 0

---
+++ javascript-dom_manipulation/0-script.js
@@ -0,0 +1,5 @@
+// Seleccione el elemento del encabezado usando document.querySelector
+var headerElement = document.querySelector('header');
+
+// Actualice el color del texto del encabezado a rojo (#FF0000)
+headerElement.style.color = '#FF0000';
