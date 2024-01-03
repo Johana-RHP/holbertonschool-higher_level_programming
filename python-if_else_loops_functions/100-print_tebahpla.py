f9404924d43a1655255dfc7c2ad645e4e2caceb5
Johana-RHP <98305125+Johana-RHP@users.noreply.github.com>
2024-01-03 16:37:28 -0500

    Delete python-if_else_loops_functions directory

--- python-if_else_loops_functions/100-print_tebahpla.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-for letter in range(90, 64, -1):
-    if letter % 2 == 0:
-        letter += 32
-        print("{:c}".format(letter), end="")
-    else:
-        print("{:c}".format(letter), end="")
--- python-if_else_loops_functions/8-uppercase.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def uppercase(str):
-    for i in range(len(str)):
-        letter = str[i]
-        if 97 <= ord(letter) and ord(letter) <= 122:
-            letter = chr(ord(letter) - 32)
-        print("{}".format(letter), end="")
-    print("")
