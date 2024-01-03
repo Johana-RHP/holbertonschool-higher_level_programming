b493b7a378110b87a97a01b6f428f88c91677f22
Johana-RHP <98305125+Johana-RHP@users.noreply.github.com>
2024-01-03 16:37:56 -0500

    Delete python-import_modules directory

--- python-import_modules/3-infinite_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-if __name__ == '__main__':
-    from sys import argv
-    length = len(argv)
-    add = 0
-    for i in range(1, length):
-        add += int(argv[i])
-
-    print("{:d}".format(add))
