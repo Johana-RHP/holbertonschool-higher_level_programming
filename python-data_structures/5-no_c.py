5b91f361ffae60b28e6bd3c8adc2e13090906e0b
Johana-RHP <98305125+Johana-RHP@users.noreply.github.com>
2024-01-03 16:38:22 -0500

    Delete python-data_structures directory

--- python-data_structures/5-no_c.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def no_c(my_string):
-    new_string_1 = my_string.translate({ord('c'): None})
-    new_string_2 = new_string_1.translate({ord('C'): None})
-    return new_string_2
