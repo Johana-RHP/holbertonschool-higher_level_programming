468537346e70e1c8956636ad3f5d1087ba7d6f34
Johana-RHP <johana.rosicel@gmail.com>
2024-01-15 03:00:20 +0000

    task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-"""
-Contains method lookup that returns list of object's attribute and methods
-"""
+"""return attributes and methods"""
 
 
 def lookup(obj):
-    """returns list of object's attribute and methods"""
+    """return attributes and methods"""
     return dir(obj)
